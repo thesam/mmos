f915debcaf6e697e97684d431a6874f41f9e3b0b
Samuel Moritz <samuel.moritz@gmail.com>
2016-03-31 21:08:02 +0200

    Add MersenneTwister

--- core/src/se/timberline/mmos/server/LocalServer.java
+++ core/src/se/timberline/mmos/server/LocalServer.java
@@ -1,5 +1,6 @@
 package se.timberline.mmos.server;
 
+import org.apache.commons.math3.random.MersenneTwister;
 import se.timberline.mmos.api.Message;
 import se.timberline.mmos.api.PlanetMessage;
 import se.timberline.mmos.api.PositionMessage;
@@ -16,6 +17,10 @@ public class LocalServer {
     public void connect(Consumer<Message> messageConsumer) {
         messageConsumers.add(messageConsumer);
         messageConsumer.accept(new PositionMessage(0,0));
+        int[] sectorSeed = {0, 0};
+        MersenneTwister twister = new MersenneTwister(sectorSeed);
+        System.out.println(twister.nextInt());
+        System.out.println(twister.nextInt());
         messageConsumer.accept(new PlanetMessage(10,10,"Foo"));
     }
 
